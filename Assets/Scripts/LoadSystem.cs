e8ff9c4be5a34a6db71955429e059baf96c2edc8
Matheus Henrique Vicente Faria <matheushe_vicen@outlook.com>
2023-08-03 22:18:58 -0300

    Add a exit function in Load System
    Add a exit function in Load System

--- Assets/Scripts/LoadSystem.cs
+++ Assets/Scripts/LoadSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,6 +13,15 @@ namespace GameSystem
         {
             //Load the game Scene
             SceneManager.LoadScene(1);
+        }
+
+        public void OnAppExit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit(); // original code to quit Unity player
+#endif
         }
     }
 
